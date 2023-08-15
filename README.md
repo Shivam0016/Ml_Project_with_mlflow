12bfd3a2db8d8803f7711a19927845995ff3bfa8
Shivam0016 <shivam0016@gmail.com>
2023-08-16 00:28:42 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Ml_Project_with_mlflow
 Machine Learning Project with implementation of MLFlow
+
+
+## Workflows
+1.Update config.yaml
+2.Update schema.yaml
+3.Update params.yaml
+4.update entity
+5.update the configuration manager in src confg
+6.update the components
+7.update the pipeline
+8.update the main.py
+9.update the app.yaml
--- src/mlproject/utils/common.py
+++ src/mlproject/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mlproject import logger
+from src.mlproject import logger
 import json
 import joblib
 from ensure import ensure_annotations
@@ -16,6 +16,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         with open(path_to_yaml) as yaml_file:
             content=yaml.safe_load(yaml_file)
             logger.info(f"yaml File: {path_to_yaml} loaded successfully")
+           
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file empty")
@@ -23,7 +24,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise e 
     
 @ensure_annotations
-def create_directores(path_to_directories: list, verbose=True):
+def create_directories(path_to_directories: list, verbose=True):
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
